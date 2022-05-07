ebd707f7449583f4386ad9a45f12de184b06d8b7
Justin Taylor <themrpotatohead@gmail.com>
2022-05-07 07:50:58 -0600

    Add site title

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
                 link({'rel': 'icon', 'href': 'mic-fill.png'}),
                 script({'src': "talks.js"}),
                 script({'src': "scripts.js"}),
+                title('General Conference Talk Randomizer')
             ]),
             body([
                 div({'class': 'container'}, children=[
@@ -34,10 +35,16 @@ if __name__ == '__main__':
                     p(input_({'type': 'button', 'onclick': 'set_talk_link()', 'value': 'Change talk link'})),
                     script('set_talk_link();')
                 ]),
+                div({'class': 'container'}, hr()),
                 div({'class': 'container'}, children=[
-                    p({'class': 'text-end'}, [
-                        a({'href': 'https://github.com/thetaylors/pyConferenceTalkRandomizer'},
-                          '''<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
+                    div({'class': 'row justify-content-between'}, [
+                        div({'class': 'col-6'}, children=['This website is NOT an official website of ',
+                                                          a({'href': 'https://www.churchofjesuschrist.org/'},
+                                                            'The Church of Jesus Christ of Latter-day Saints.')
+                                                          ]),
+                        div({'class': 'col-1'}, [
+                            a({'href': 'https://github.com/thetaylors/pyConferenceTalkRandomizer'},
+                              '''<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                           class="bi bi-github" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 
                           5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48
                           -.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07
@@ -46,15 +53,8 @@ if __name__ == '__main__':
                           -.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73
                           .54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42
                           -3.58-8-8-8z"/></svg>''' + '&nbsp;Source')
+                        ])
                     ])
-                ]
-                    ),
-                div({'class': 'container'}, children=[
-                    hr(),
-                    p(children=['This website is NOT an official website of ',
-                                a({'href': 'https://www.churchofjesuschrist.org/'},
-                                  'The Church of Jesus Christ of Latter-day Saints.')
-                                ])
                 ])
             ])
         ])
